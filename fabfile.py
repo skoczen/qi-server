5acdfae16faaf32fc3eec60f4811e426281ba6b4
Steven Skoczen <steven@quantumimagery.com>
2011-04-07 01:06:24 -0700

    more accurate fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,12 @@ from qi_toolkit.fabbase import *
 setup_env_centos("qiserver","root",
                 initial_settings={
                     'staging_hosts':[],
-                    'production_hosts':['50.17.47.150'],
+                    'production_hosts':['qistaging.com'],
                     'production_db_hosts':[],
                     'staging_db_hosts':[]
                 }, 
                 overrides={
-                    'git_origin':"http://50.17.47.150/qiserver.git",
+                    'git_origin':"http://git@github.com:skoczen/qi-server.git",
                     # 'dry_run':True,
                     'local_working_path':"~/workingCopy/qi/qiserver.git",
                 },
