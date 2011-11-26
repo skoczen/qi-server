8616fe5e001efa09f05e4aba5cb79ad8a9786943
Steven Skoczen <steven@quantumimagery.com>
2011-11-26 13:55:14 -0800

    updated bootstrap

--- config.dist/bootstrap_ubuntu_server.sh
+++ config.dist/bootstrap_ubuntu_server.sh
@@ -1,56 +1,23 @@
-#!/bin/bash
-# do this by hand
-# cd /var/www
-# aptitude install -y git
-# git clone git@github.com:skoczen/qi-server.git qiserver.git
+aptitude install -y percona-server-client-5.1 percona-server-common percona-server-server-5.1 libmysqlclient-dev libpq-dev sqlite3 python2.7 python-setuptools python2.7-dev build-essential xvfb firefox htop mercurial libjpeg-dev zlib1g-dev  libjpeg62 libjpeg62-dev lib-zlib libfreetype6 libfreetype6-dev apache2-utils redis-server memcached libcurl4-dev libidn11 libidn11-dev libdap-dev librtmp-dev postgresql upstart chkconfig libmysql++-dev htop libcurl curl libcurl4-gnutls-dev postgresql
+sudo apt-get install -y ruby rubygems ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8
+sudo apt-get install -y libreadline-ruby1.8 libruby1.8 libopenssl-ruby
+sudo apt-get install -y libxslt-dev libxml2-dev
+gem install nokogiri
+gem install backup
+gem install fog
 
-# has nginx, memcached, mysql installed.
-aptitude install -y git python-setuptools python2.6-dev python-virtualenv python-pip libmysqld-dev sqlite libgmp3-dev  mercurial libjpeg62-dev zlib1g-dev libfreetype6-dev postgresql postgresql-client python-psycopg2 postgresql-contrib
-
-# eventually rabbitmq-server
-# eventuall also memcached will get its own server, but not right now.
-cd /etc/init.d; wget https://github.com/ask/celery/raw/master/contrib/generic-init.d/celeryd --no-check-certificate; chmod +x celeryd
-sed '1d' celeryd > celeryd.tmp
-echo "# chkconfig: 2345 20 80" > celeryd
-echo "# description: The Celery start-stop-script" >> celeryd
-cat celeryd.tmp >> celeryd
-rm celeryd.tmp
-# rabbitmqctl add_user user pass
-# rabbitmqctl add_vhost vhost
-# rabbitmqctl set_permissions -p vhost user ".*" ".*" ".*"
-echo 'alias python=python26' >> ~/.bashrc
-echo 'VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.6' >> ~/.bashrc
-source ~/.bashrc
+easy_install-2.7 pip
+echo 'VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7' >> ~/.profile
+source ~/.profile
 mkdir /var/www
-mkdir /var/backups
+rm -rf /var/www/*
 pip install --upgrade pip virtualenv virtualenvwrapper
 cd ~;mkdir .virtualenvs
 chmod +x /root
-echo 'source /usr/local/bin/virtualenvwrapper.sh' >> .bashrc
-source ~/.bashrc
+echo 'source /usr/local/bin/virtualenvwrapper.sh' >> .profile
+source ~/.profile
+
 cd /var/www
-# git clone http://qiserver.skoczen.webfactional.com/qiserver.git qiserver.git
-cd qiserver.git;git checkout master
-cat /var/www/qiserver.git/config.dist/authorized_keys >> ~/.ssh/authorized_keys
-mkvirtualenv qiserver
-echo 'cd /var/www/qiserver.git' >> ~/.virtualenvs/qiserver/bin/postactivate
-workon qiserver
-pip install -r requirements.txt 
-# echo 'mysql_config = /usr/bin/mysql_config' >> ~/.virtualenvs/qiserver/build/mysql-python/site.cfg
-# pip install -r requirements.txt 
-mkdir /var/log/celery
-mv /etc/default/celeryd /etc/default/celeryd.bak
-ln -s /var/www/qiserver.git/config.dist/celeryd /etc/default/celeryd;chmod +x /etc/default/celeryd
+git clone git@github.com:skoczen/qi-server.git qiserver.git
 mv /etc/nginx/nginx.conf /etc/default/nginx.conf.bak
 ln -s /var/www/qiserver.git/config.dist/nginx.conf /etc/nginx/nginx.conf
-
-
-# chkconfig --add memcached
-# chkconfig --add rabbitmq-server
-# # chkconfig --add celeryd
-# chkconfig --add nginx
-# service memcached start
-# service rabbitmq-server start
-# # service celeryd start
-# service nginx start
-
